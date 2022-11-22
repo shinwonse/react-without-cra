4b0214a5674ee7ac8c6baf50850f84decb0a34bb
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-23 00:41:27 +0900

    feat: webpack 설정 수정 -> output path 삭제

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
